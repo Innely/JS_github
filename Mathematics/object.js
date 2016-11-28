819f00e86524d8a14732d71423231c615f319f65
Inna Lazarenko <innalazar@gmail.com>
2016-11-28 15:33:19 +0200

    after review

--- Mathematics/object.js
+++ Mathematics/object.js
@@ -1,59 +1,50 @@
 var Mathematics = {
     // 1. The construction of A to Grade B
     pow: function (a, b) {
-      var power = a;
+      var result = a;
       for (var i = 1; i < b; i++) {
-        power = power * a;
+        result = result * a;
       }
-      return power;
+      return result;
     },
 
     // 2. Return minimum number
     min: function (array) {
-      // Check on the number of entering array
-      for (var i = 0; i < array.length; i++) {
-        if (typeof array[i] !== 'number') {
-          console.log('ERROR: Not a number');
-          return;
-        }
-      }
-
-      var mini = array[0];
-      for (var j = 0; j < array.length; j++) {
-        if (mini > array[j + 1]) {
-          mini = array[j + 1];
-        }
-      }
-      return mini;
-
-    },
-
-    // 3. Return maximum number
-    max: function (array) {
-      // Check on the number of entering array
+      var result = array[0];
       for (var i = 0; i < array.length; i++) {
         if (typeof array[i] !== 'number') {
           console.warn('ERROR: Not a number');
           return;
         }
-      }
-
-      var maxi = array[0];
-      for (var j = 0; j < array.length; j++) {
-        if (maxi < array[j + 1]) {
-          maxi = array[j + 1];
+        if (result > array[i + 1]) {
+          result = array[i + 1];
         }
       }
-      return maxi;
+      return result;
+    },
+
+    // 3. Return maximum number
+    max: function (array) {
+      var result = array[0];
+      for (var i = 0; i < array.length; i++) {
+        if (typeof array[i] !== 'number') {
+          console.warn('ERROR: Not a number');
+          return;
+        }
+        if (result < array[i + 1]) {
+          result = array[i + 1];
+        }
+      }
+      return result;
     },
 
     // 4. Calculation of the factorial A
     factorial: function (a) {
-      var fact = 1;
+      var result = 1;
       for (var i = 1; i < a; i++) {
-        fact = fact * (i + 1);
+        result = result * (i + 1);
       }
-      return fact;
+      return result;
     },
 
     // 5. Constant of object
